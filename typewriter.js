ba1f8950bd6e2deda94b7aa49f3635c9c167be56
Bilqis Ahmed <bilqis.ali32@gmail.com>
2023-10-25 20:50:14 +0000

    typewriter update

--- typewriter.js
+++ typewriter.js
@@ -1,76 +1,16 @@
 const sentence = " hello there from lhl";
-
+let delay = 0;
 for (const char of sentence) {
-  process.stdout.write(char);
+  // process.stdout.write(char)
+
+  setTimeout(() =>
+    process.stdout.write(char + "\n")
+    , delay += 1000);
+
 }
 
 
 
-setTimeout(() => {
-  console.log("h");
-}, 1000);
 
-setTimeout(() => {
-  console.log("e");
-}, 2000);
 
-setTimeout(() => {
-  console.log("l");
-}, 3000);
-
-setTimeout(() => {
-  console.log("l");
-}, 4000);
-
-setTimeout(() => {
-  console.log("o");
-}, 5000);
-
-setTimeout(() => {
-  console.log("t");
-}, 6000);
-
-setTimeout(() => {
-  console.log("h");
-}, 7000);
-setTimeout(() => {
-  console.log("e");
-}, 8000);
-setTimeout(() => {
-  console.log("r");
-}, 9000);
-setTimeout(() => {
-  console.log("e");
-}, 10000);
-setTimeout(() => {
-  console.log("f");
-}, 11000);
-setTimeout(() => {
-  console.log("r");
-}, 12000);
-setTimeout(() => {
-  console.log("o");
-}, 13000);
-setTimeout(() => {
-  console.log("m");
-}, 14000);
-
-setTimeout(() => {
-  console.log("l");
-}, 750);
-
-setTimeout(() => {
-  console.log("h");
-}, 800);
-
-setTimeout(() => {
-  console.log("l");
-}, 850);
-   
-   
- 
- 
- 
- 
- 
 
